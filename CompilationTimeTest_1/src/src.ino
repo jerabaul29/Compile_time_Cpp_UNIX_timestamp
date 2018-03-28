296e13d11d735bb6ddcd00d397278766a1105a93
jerabaul29 <jean.rblt@gmail.com>
2018-03-29 00:22:53 +0200

    working unix timestamps

--- CompilationTimeTest_1/src/src.ino
+++ CompilationTimeTest_1/src/src.ino
@@ -1,7 +1,38 @@
 #include <Arduino.h>
 
+
+/*
+  NOTE: to make a unix timestamp in Python:
+
+  import time
+  import datetime
+  s = "29/03/2018 00:09:15"
+  time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y %H:%M:%S").timetuple())
+
+*/
+
+#define UNIX_TIME_OBJECTIVE 1522278000UL
+
+
 void setup() {
-    // put your setup code here, to run once:
+    delay(1000);
+
+    Serial.begin(9600);
+    delay(100);
+    Serial.println(F(__DATE__));
+    Serial.println(F(__TIME__));
+
+    // just to check that the right type
+    const static unsigned long compile_time = COMPILE_TIME;
+
+    Serial.print(F("Compile time: "));
+    Serial.println(COMPILE_TIME);
+
+    Serial.print(F("Objective time: "));
+    Serial.println(UNIX_TIME_OBJECTIVE);  
+
+    Serial.print(F("Seconds to objective: "));
+    Serial.println(UNIX_TIME_OBJECTIVE - COMPILE_TIME);  
 }
 
 void loop() {
